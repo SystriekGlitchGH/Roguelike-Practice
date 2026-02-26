290d5b60e5683abfbf407d50b1e52c2d5a20e3e4
Arcturus <systriekglitchyt@gmail.com>
2026-02-26 00:54:13 -0800

    you can now attack in all directions with proper hitbox AND hitbox can be seen in debug at all times

--- Assets/Scripts/MonoScripts/PlayerMovement.cs
+++ Assets/Scripts/MonoScripts/PlayerMovement.cs
@@ -22,7 +22,15 @@ public class PlayerMovement : MonoBehaviour
     public Vector2 attackSize;
     public float attackDistance;
 
-    private float attackDirectionX, attackDirectionY;
+    [Header("Debugging Tools")]
+    [SerializeField] Transform anchorTransform;
+
+    private float attackAngle;
+    private enum Direction
+    {
+        North, South, East, West, NorthEast, NorthWest, SouthEast, SouthWest
+    }
+    private Direction playerDirection;
 
     private void Awake()
     {
@@ -39,15 +47,13 @@ public class PlayerMovement : MonoBehaviour
         // applying clamped velocity to rigidbody2d
         rb2d.linearVelocity = velocity;
         // when both direction inputs are 0, add linear damping to slow down player quickly
-        if(directionX == 0 && directionY == 0)
-            rb2d.linearDamping = friction;
-        else
-            rb2d.linearDamping = 0;
+        if(directionX == 0 && directionY == 0) rb2d.linearDamping = friction;
+        else rb2d.linearDamping = 0;
     }
     private void Update()
     {
-        attackDirectionX = directionX;
-        attackDirectionY = directionY;
+        FindDirection();
+        FindAngle();
     }
     public void Move(InputAction.CallbackContext ctx)
     {
@@ -62,46 +68,71 @@ public class PlayerMovement : MonoBehaviour
             return;
         if(ctx.performed)
         {
-            RaycastHit2D hit = Physics2D.BoxCast(new Vector2(transform.position.x,transform.position.y+attackDistance), attackSize, 0, Vector2.zero);
+            //RaycastHit2D hit = Physics2D.BoxCast(new Vector2(transform.position.x,transform.position.y+attackDistance), attackSize, attackAngle, Vector2.zero);
+            RaycastHit2D hit = MakeBoxCastAttack();
             if (hit && hit.collider.TryGetComponent(out EnemyMovement enemy))
             {
                 Debug.Log("attack succesful");
-                enemy.Die();
+                //enemy.Die();
             }
         }
     }
 
+    private void FindAngle()
+    {
+        if(playerDirection == Direction.North) attackAngle = 0;
+        if(playerDirection == Direction.South) attackAngle = 180;
+        if(playerDirection == Direction.East) attackAngle = -90;
+        if(playerDirection == Direction.West) attackAngle = 90;
+        if(playerDirection == Direction.NorthEast) attackAngle = -45;
+        if(playerDirection == Direction.NorthWest) attackAngle = 45;
+        if(playerDirection == Direction.SouthEast) attackAngle = -135;
+        if(playerDirection == Direction.SouthWest) attackAngle = 135;
+    }
+    private void FindDirection()
+    {
+        //north
+        if(directionX == 0 && directionY > 0) playerDirection = Direction.North;
+        //south
+        if(directionX == 0 && directionY < 0) playerDirection = Direction.South;
+        //east
+        if(directionX > 0 && directionY == 0) playerDirection = Direction.East;
+        //west
+        if(directionX < 0 && directionY == 0) playerDirection = Direction.West;
+        //northeast
+        if(directionX > 0 && directionY > 0) playerDirection = Direction.NorthEast;
+        //northwest
+        if(directionX < 0 && directionY > 0) playerDirection = Direction.NorthWest;
+        //southeast
+        if(directionX > 0 && directionY < 0) playerDirection = Direction.SouthEast;
+        //southwest
+        if(directionX < 0 && directionY < 0) playerDirection = Direction.SouthWest;
+    }
+    private RaycastHit2D MakeBoxCastAttack()
+    {
+        if(playerDirection == Direction.North)
+            return Physics2D.BoxCast(new Vector2(transform.position.x,transform.position.y+attackDistance), attackSize, attackAngle, Vector2.zero);
+        if(playerDirection == Direction.South)
+            return Physics2D.BoxCast(new Vector2(transform.position.x,transform.position.y-attackDistance), attackSize, attackAngle, Vector2.zero);
+        if(playerDirection == Direction.East)
+            return Physics2D.BoxCast(new Vector2(transform.position.x+attackDistance,transform.position.y), attackSize, attackAngle, Vector2.zero);
+        if(playerDirection == Direction.West)
+            return Physics2D.BoxCast(new Vector2(transform.position.x-attackDistance,transform.position.y), attackSize, attackAngle, Vector2.zero);
+        if(playerDirection == Direction.NorthEast)
+            return Physics2D.BoxCast(new Vector2(transform.position.x+Mathf.Sqrt(attackDistance),transform.position.y+Mathf.Sqrt(attackDistance)), attackSize, attackAngle, Vector2.zero);
+        if(playerDirection == Direction.NorthWest)
+            return Physics2D.BoxCast(new Vector2(transform.position.x-Mathf.Sqrt(attackDistance),transform.position.y+Mathf.Sqrt(attackDistance)), attackSize, attackAngle, Vector2.zero);
+        if(playerDirection == Direction.SouthEast)
+            return Physics2D.BoxCast(new Vector2(transform.position.x+Mathf.Sqrt(attackDistance),transform.position.y-Mathf.Sqrt(attackDistance)), attackSize, attackAngle, Vector2.zero);
+        if(playerDirection == Direction.SouthWest)
+            return Physics2D.BoxCast(new Vector2(transform.position.x-Mathf.Sqrt(attackDistance),transform.position.y-Mathf.Sqrt(attackDistance)), attackSize, attackAngle, Vector2.zero);
+        return Physics2D.BoxCast(new Vector2(transform.position.x,transform.position.y), attackSize, attackAngle, Vector2.zero);
+    }
     private void OnDrawGizmos()
     {   
-        //up
-        if(attackDirectionX == 0 && attackDirectionY > 0)
-        {
-            Gizmos.DrawLine(new Vector2(transform.position.x-(attackSize.x/2),transform.position.y+(attackDistance/2)),new Vector2(transform.position.x+(attackSize.x/2),transform.position.y+(attackDistance/2)));
-            Gizmos.DrawLine(new Vector2(transform.position.x-(attackSize.x/2),transform.position.y+(attackDistance/2)),new Vector2(transform.position.x-(attackSize.x/2),transform.position.y+(attackDistance/2)+attackSize.y));
-        }
-        //right
-        if(attackDirectionX > 0 && attackDirectionY == 0)
-        {
-            Gizmos.DrawLine(new Vector2(transform.position.x+(attackDistance/2),transform.position.y+(attackSize.x/2)),new Vector2(transform.position.x+(attackDistance/2),transform.position.y-(attackSize.x/2)));
-            Gizmos.DrawLine(new Vector2(transform.position.x+(attackDistance/2),transform.position.y+(attackSize.x/2)),new Vector2(transform.position.x+(attackDistance/2)+attackSize.y,transform.position.y+(attackSize.x/2)));
-        }
-        //down
-        if(attackDirectionX == 0 && attackDirectionY < 0)
-        {
-            Gizmos.DrawLine(new Vector2(transform.position.x-(attackSize.x/2),transform.position.y-(attackDistance/2)),new Vector2(transform.position.x+(attackSize.x/2),transform.position.y-(attackDistance/2)));
-            Gizmos.DrawLine(new Vector2(transform.position.x+(attackSize.x/2),transform.position.y-(attackDistance/2)),new Vector2(transform.position.x+(attackSize.x/2),transform.position.y-(attackDistance/2)-attackSize.y));
-        }
-        //left
-        if(attackDirectionX < 0 && attackDirectionY == 0)
-        {
-            Gizmos.DrawLine(new Vector2(transform.position.x-(attackDistance/2),transform.position.y+(attackSize.x/2)),new Vector2(transform.position.x-(attackDistance/2),transform.position.y-(attackSize.x/2)));
-            Gizmos.DrawLine(new Vector2(transform.position.x-(attackDistance/2),transform.position.y-(attackSize.x/2)),new Vector2(transform.position.x-(attackDistance/2)-attackSize.y,transform.position.y-(attackSize.x/2)));
-        }
-        //upright
-        if(attackDirectionX > 0 && attackDirectionY > 0)
-        {
-            Gizmos.DrawLine(new Vector2(transform.position.x+(attackDistance/4),transform.position.y+(attackDistance/2f)),new Vector2(transform.position.x,transform.position.y));
+        Gizmos.matrix = anchorTransform.localToWorldMatrix;
+        anchorTransform.eulerAngles = new Vector3(0,0,attackAngle);
+        Gizmos.DrawWireCube(new Vector2(0,attackDistance), attackSize);
         
-        }
     }
 }
