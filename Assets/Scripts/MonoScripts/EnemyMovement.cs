9dc0960da11e600791686da48a9c92f4bd7e10ab
Arcturus <systriekglitchyt@gmail.com>
2026-02-27 22:44:56 -0800

    updated movement stats and knockback works now

--- Assets/Scripts/MonoScripts/EnemyMovement.cs
+++ Assets/Scripts/MonoScripts/EnemyMovement.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using Unity.VisualScripting;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
@@ -15,14 +16,26 @@ public class EnemyMovement : MonoBehaviour
     public float friction;
     // variables to help with movement
     private float distance;
+    private bool hasKnockback;
+    public float knockbackTime;
 
+    private void Start()
+    {
+        rb2d.linearDamping = friction;
+    }
     private void FixedUpdate()
     {
         if(target != null)
         {
+            Debug.Log(PlayerDirection(target.transform.position));
+            if (hasKnockback)
+            {
+                return;
+            }
             distance = PlayerDistance(target.transform.position);
             if(distance > 2)
             {
+                rb2d.linearDamping = 0;
                 Vector2 newVelocity = PlayerDirection(target.transform.position)*acceleration;
                 rb2d.AddForce(newVelocity);
                 Vector2 velocity = Vector2.ClampMagnitude(new(rb2d.linearVelocity.x, rb2d.linearVelocity.y), topSpeed);
@@ -45,7 +58,15 @@ public class EnemyMovement : MonoBehaviour
     }
     public void Hit(Vector2 playerPos, float knockback)
     {
+        StartCoroutine(Knockback());
         rb2d.AddForce(-PlayerDirection(playerPos)*knockback,ForceMode2D.Impulse);
+    }
+
+    private IEnumerator Knockback()
+    {
+        hasKnockback = true;
+        yield return new WaitForSeconds(knockbackTime);
+        hasKnockback = false;
     }
 
     //movement help methods
@@ -55,6 +76,6 @@ public class EnemyMovement : MonoBehaviour
     }
     private Vector2 PlayerDirection(Vector2 playerPos)
     {
-        return playerPos - (Vector2)transform.position;
+        return (playerPos - (Vector2)transform.position).normalized;
     }
 }
